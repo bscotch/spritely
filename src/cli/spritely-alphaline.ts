cdf1fbc5371d40b81ecb721353d264fd9ebc90e0
Adam Coster <adam@bscotch.net>
2020-10-09 16:10:46 -0500

    feat: Add watcher option to sprite-alphaline command.

--- src/cli/spritely-alphaline.ts
+++ src/cli/spritely-alphaline.ts
@@ -2,6 +2,20 @@
 import cli from "commander";
 import {Spritely} from "../lib/Spritely";
 import {addGeneralOptions, getSpriteDirs} from "./util";
+import { toPosixPath } from "@bscotch/utility";
+import path from "path";
+import chokidar from "chokidar";
+
+async function alphalineSpriteDir(spriteDir:string){
+  try{
+    const sprite = new Spritely(spriteDir);
+    await sprite.alphaline();
+    console.log(`Alphalined sprite "${spriteDir}"`);
+  }
+  catch(err){
+    console.log(`Sprite alphaline failed for "${spriteDir}"`,err?.message);
+  }
+}
 
 async function alphalineSprites(){
   addGeneralOptions(cli.description('Spritely: Alphaline'))
@@ -10,14 +24,22 @@ async function alphalineSprites(){
   // Get all directories starting in folder
   const spriteDirs = getSpriteDirs(cli);
   for(const spriteDir of spriteDirs){
-    try{
-      const sprite = new Spritely(spriteDir);
-      await sprite.alphaline();
-      console.log(`Alphalined sprite "${spriteDir}"`);
-    }
-    catch(err){
-      console.log(`Sprite alphaline failed for "${spriteDir}"`,err?.message);
-    }
+    await alphalineSpriteDir(spriteDir);
+  }
+
+  // watcher
+  if(cli.watch){
+    const glob = toPosixPath(
+      cli.recursive
+        ? path.join(cli.folder,'**/*.png')
+        : path.join(cli.folder,'*.png')
+    );
+    const rerunOnDir = (filepath:string)=>alphalineSpriteDir(path.dirname(filepath));
+    chokidar
+      .watch(glob,{ignoreInitial:true,interval:100})
+      .on("ready",()=>console.log(`Watching for sprite changes matching pattern: ${glob}`))
+      .on('add',rerunOnDir)
+      .on('change',rerunOnDir);
   }
 }
 
