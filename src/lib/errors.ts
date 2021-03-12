1e7bb7886acc4893293fc83a6324080d81b2e4e3
Adam Coster <adam@bscotch.net>
2021-03-11 18:23:40 -0600

    feat: Add extra logging context for file system failures, since these are still plaguing production.

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,37 +1,50 @@
-import "source-map-support/register";
-
-import fs from "fs-extra";
+import 'source-map-support/register';
+import fs from 'fs-extra';
 
 export class SpritelyError extends Error {
-  constructor(message:string){
+  constructor(message: string) {
     super(message);
     this.name = 'SpritelyError';
-    Error.captureStackTrace(this,this.constructor);
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
 /** Throw an error if `claim` is falsey */
-export function assert(claim:any,messageIfFalsey:string){
-  if(!claim){
+export function assert(claim: any, messageIfFalsey: string): asserts claim {
+  if (!claim) {
     throw new SpritelyError(messageIfFalsey);
   }
 }
 
-export function assertDirectoryExists(directory:string){
-  assert(
-    fs.existsSync(directory),
-    `${directory} does not exist`
-  );
-  assert(
-    fs.statSync(directory).isDirectory(),
-    `${directory} is not a folder`
-  );
+export function assertDirectoryExists(directory: string) {
+  try {
+    assert(fs.existsSync(directory), `${directory} does not exist`);
+    assert(
+      fs.statSync(directory).isDirectory(),
+      `${directory} is not a folder`
+    );
+  } catch (err) {
+    if (err instanceof SpritelyError) {
+      throw err;
+    } else {
+      console.log(
+        'External error: Could not check for existence of directory.'
+      );
+      throw err;
+    }
+  }
 }
 
-export function assertNonEmptyArray(something:any,message="Expected non-empty array."){
+export function assertNonEmptyArray(
+  something: any,
+  message = 'Expected non-empty array.'
+) {
   assert(Array.isArray(something) && something.length, message);
 }
 
-export function assertNumberGreaterThanZero(something:any,message="Expected number greater than zero."){
-  assert(typeof something == 'number' && something>0, message);
+export function assertNumberGreaterThanZero(
+  something: any,
+  message = 'Expected number greater than zero.'
+) {
+  assert(typeof something == 'number' && something > 0, message);
 }
--- src/lib/utility.ts
+++ src/lib/utility.ts
@@ -1,19 +1,19 @@
-import { wait } from "@bscotch/utility";
-import crypto from "crypto";
-import fs from "fs-extra";
+import { wait } from '@bscotch/utility';
+import crypto from 'crypto';
+import fs from 'fs-extra';
 import {
   listFilesByExtensionSync,
   listFoldersSync,
   listPathsSync,
   removeEmptyDirsSync,
-} from "@bscotch/utility";
+} from '@bscotch/utility';
 
 const FILE_FUNCTION_RETRY_WAIT_MILLIS = 100;
 
 /**
  * From Lodash {@link https://github.com/lodash/lodash/blob/master/clamp.js}
  */
-export function clamp(number:number, lower:number, upper:number) {
+export function clamp(number: number, lower: number, upper: number) {
   number = +number;
   lower = +lower;
   upper = +upper;
@@ -26,22 +26,19 @@ export function clamp(number:number, lower:number, upper:number) {
   return number;
 }
 
-export function randomHex (bytes=8): Promise<string>{
-  return new Promise((resolve,reject)=>{
-    crypto.randomBytes(bytes,(err,buffer)=>{
-      if(err){
+export function randomHex(bytes = 8): Promise<string> {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(bytes, (err, buffer) => {
+      if (err) {
         return reject(err);
       }
-      return resolve( buffer.toString('hex'));
+      return resolve(buffer.toString('hex'));
     });
   });
 }
 
-export function sha256 (data: crypto.BinaryLike){
-  return crypto
-    .createHash('sha256')
-    .update(data)
-    .digest('hex');
+export function sha256(data: crypto.BinaryLike) {
+  return crypto.createHash('sha256').update(data).digest('hex');
 }
 
 type Unwrapped<T> = T extends PromiseLike<infer U> ? Unwrapped<U> : T;
@@ -50,47 +47,48 @@ type Unwrapped<T> = T extends PromiseLike<infer U> ? Unwrapped<U> : T;
  * Wrap a file function so that it can auto-retry on EBUSY and EPERM errors
  * (these are caused when Dropbox tries to do something on the same files).
  */
-function makeRetriable<
-  FileOpFunction extends ((...args:any[])=>any)
->(fileOpFunction:FileOpFunction){
-
-  const retriableFunction = async (...args: Parameters<FileOpFunction>):Promise<Unwrapped<ReturnType<FileOpFunction>>> =>{
+function makeRetriable<FileOpFunction extends (...args: any[]) => any>(
+  fileOpFunction: FileOpFunction
+) {
+  const retriableFunction = async (
+    ...args: Parameters<FileOpFunction>
+  ): Promise<Unwrapped<ReturnType<FileOpFunction>>> => {
     let fails = 0;
-    try{
+    try {
       return await fileOpFunction(...args);
-    }
-    catch(err){
+    } catch (err) {
       fails++;
       const message = err?.message;
-      const isPotentialDropboxError = message?.startsWith("EBUSY") ||
-        message?.startsWith("EPERM");
-      if(fails<10 && isPotentialDropboxError){
+      const isPotentialDropboxError =
+        message?.startsWith('EBUSY') || message?.startsWith('EPERM');
+      if (fails < 10 && isPotentialDropboxError) {
         await wait(FILE_FUNCTION_RETRY_WAIT_MILLIS);
         return retriableFunction(...args);
       }
+      console.log(`${fileOpFunction.name} failed ${fails} times.`);
       throw err;
     }
   };
   return retriableFunction;
 }
 
-type WriteFileFn = (file:string,data:string|Buffer)=>Promise<void>;
-type FileMutateFun = (path:string)=>Promise<void>;
-type CopyFn = (from:string,to:string)=>Promise<void>;
-type ExistsFn = (path:string)=>Promise<boolean>;
+type WriteFileFn = (file: string, data: string | Buffer) => Promise<void>;
+type FileMutateFun = (path: string) => Promise<void>;
+type CopyFn = (from: string, to: string) => Promise<void>;
+type ExistsFn = (path: string) => Promise<boolean>;
 
 export const fsRetry = {
-  emptyDir:   makeRetriable<FileMutateFun>(fs.emptyDir),
-  ensureDir:  makeRetriable<FileMutateFun>(fs.ensureDir),
-  listFilesByExtension:  makeRetriable(listFilesByExtensionSync),
+  emptyDir: makeRetriable<FileMutateFun>(fs.emptyDir),
+  ensureDir: makeRetriable<FileMutateFun>(fs.ensureDir),
+  listFilesByExtension: makeRetriable(listFilesByExtensionSync),
   listFolders: makeRetriable(listFoldersSync),
-  listPaths:   makeRetriable(listPathsSync),
-  move:        makeRetriable<CopyFn>(fs.move),
-  pathExists:  makeRetriable<ExistsFn>(fs.pathExists),
-  remove:      makeRetriable<FileMutateFun>(fs.remove),
+  listPaths: makeRetriable(listPathsSync),
+  move: makeRetriable<CopyFn>(fs.move),
+  pathExists: makeRetriable<ExistsFn>(fs.pathExists),
+  remove: makeRetriable<FileMutateFun>(fs.remove),
   removeEmptyDirs: makeRetriable(removeEmptyDirsSync),
-  rmdir:      makeRetriable<FileMutateFun>(fs.rmdir),
-  writeFile:  makeRetriable<WriteFileFn>(fs.writeFile),
-  stat: makeRetriable<(path:string)=>Promise<fs.Stats>>(fs.stat),
-  copyFile: makeRetriable<CopyFn>(fs.copyFile)
+  rmdir: makeRetriable<FileMutateFun>(fs.rmdir),
+  writeFile: makeRetriable<WriteFileFn>(fs.writeFile),
+  stat: makeRetriable<(path: string) => Promise<fs.Stats>>(fs.stat),
+  copyFile: makeRetriable<CopyFn>(fs.copyFile),
 } as const;
