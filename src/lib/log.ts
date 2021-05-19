2004183c5cef7ae5f7030f7447fb6ce569ec92c5
Adam Coster <adam@bscotch.net>
2021-05-19 09:55:57 -0500

    feat: Add a logging utility for prettier STDOUT logs.

---
+++ src/lib/log.ts
@@ -0,0 +1,48 @@
+import chalk from 'chalk';
+
+const logLevelColors = {
+  debug: 'bgGray',
+  info: 'bgGreen',
+  warning: 'bgYellow',
+  error: 'bgRed',
+} as const;
+
+type LogLevel = keyof typeof logLevelColors;
+
+function getTimestamp() {
+  const now = new Date();
+  const hour = `${now.getHours()}`.padStart(2, '0');
+  const minute = `${now.getMinutes()}`.padStart(2, '0');
+  const seconds = `${now.getSeconds()}`.padStart(2, '0');
+  return `${hour}:${minute}:${seconds}`;
+}
+
+function log(level: LogLevel, ...stuff: any[]) {
+  const message = stuff.reduce((msg, item) => {
+    console.log(typeof item);
+    const itemAsString =
+      typeof item == 'object' ? JSON.stringify(item) : `${item}`;
+    return `${msg} ${itemAsString}`;
+  }, '');
+  const prefix = `${chalk.gray(getTimestamp())} ${level.toUpperCase()} `;
+  const colorer = chalk.black[logLevelColors[level]];
+  const prettyMessage = `${colorer(prefix)} ${message}`;
+  console.log(prettyMessage);
+  return prettyMessage;
+}
+
+export function debug(...stuff: any[]) {
+  return log('debug', ...stuff);
+}
+
+export function info(...stuff: any[]) {
+  return log('info', ...stuff);
+}
+
+export function error(...stuff: any[]) {
+  return log('error', ...stuff);
+}
+
+export function warning(...stuff: any[]) {
+  return log('warning', ...stuff);
+}
